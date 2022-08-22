e0096fda2c1beffbc28d362eac98f8909dc543d7
develpoer :) <mustafabadan1928@gmail.com>
2022-08-22 04:17:13 +0300

    add sidebar elements

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Sidebar from '../components/sidebar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-    Hello world!  hshjfjshjshjfshjsfh
-  </h1>
-  )
+    <div>
+     <main>
+
+        <Sidebar/>
+     </main>
+       
+    </div>
+  
+  );
 }
